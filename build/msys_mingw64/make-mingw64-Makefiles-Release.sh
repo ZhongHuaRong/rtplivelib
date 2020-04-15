5d13728f714ef7fe9826091de419eeb57c9bec48
ZhongHuaRong <1525491989@qq.com>
2020-04-15 23:20:09 +0800

    Modify CMAKE configuration

--- build/msys_mingw64/make-mingw64-Makefiles-Release.sh
+++ build/msys_mingw64/make-mingw64-Makefiles-Release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./toolChain.cmake ../../src
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./toolChain.cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" ../../src
