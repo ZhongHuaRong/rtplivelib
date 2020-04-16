7fdd14b1848e7b8a8fd9a11a51a70bf6b8d77dba
ZhongHuaRong <1525491989@qq.com>
2020-04-16 15:48:22 +0800

    add gpu info

--- src/device_manager/gpudevice.cpp
+++ src/device_manager/gpudevice.cpp
@@ -1,2 +1,102 @@
 #include "gpudevice.h"
+#include "../core/stringformat.h"
+#include <memory>
 
+#ifdef WIN64
+#include <dxgi.h>
+#include <windows.h>
+#endif
+
+namespace rtplivelib {
+
+namespace device_manager{
+
+struct Release{
+	void operator () (IUnknown * p){
+		if(p != nullptr)
+			p->Release();
+	}
+};
+
+/**
+ * @brief GPUIdentify::Get_All_GPU_Info
+ * 只适用于win8以上的系统
+ * 该类项目暂时不会用到，以后在处理
+ * @return 
+ */
+std::vector<GPUInfo> GPUIdentify::Get_All_GPU_Info() noexcept
+{
+	std::vector<GPUInfo> vector;
+#ifdef WIN64
+	using PIDXGIFactory = std::shared_ptr<IDXGIFactory>;
+	using PIDXGIAdapter = std::shared_ptr<IDXGIAdapter>;
+	using PIDXGIOutput  = std::shared_ptr<IDXGIOutput>;
+	
+	IDXGIFactory					*pFactory;
+	IDXGIAdapter					*pAdapter;
+	std::vector <PIDXGIAdapter>		vAdapters;
+	int								iAdapterNum{0};
+	
+	// 创建一个DXGI工厂  
+	HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory));
+	
+	if (FAILED(hr))
+		return vector;
+	
+	PIDXGIFactory fac_ptr(pFactory,Release());
+	// 枚举适配器  
+	while (pFactory->EnumAdapters(iAdapterNum, &pAdapter) != DXGI_ERROR_NOT_FOUND)
+	{
+		PIDXGIAdapter ptr(pAdapter,Release());
+		vAdapters.push_back(ptr);
+		++iAdapterNum;
+	}
+	
+	if(iAdapterNum == 0)
+		return vector;
+	vector.resize(iAdapterNum);
+	
+	for (size_t i = 0; i < vAdapters.size(); i++)
+	{
+		// 获取信息
+		DXGI_ADAPTER_DESC adapterDesc;
+		vAdapters[i]->GetDesc(&adapterDesc);
+		
+		vector[i].id = adapterDesc.DeviceId;
+		vector[i].video_memory = adapterDesc.DedicatedVideoMemory;
+		vector[i].system_memory = adapterDesc.DedicatedSystemMemory;
+		vector[i].name = core::StringFormat::WString2String(
+					std::wstring(adapterDesc.Description));
+		
+		// 输出设备  
+		IDXGIOutput * pOutput;
+		std::vector<PIDXGIOutput> vOutputs;
+		// 输出设备数量  
+		int iOutputNum = 0;
+		while (vAdapters[i]->EnumOutputs(iOutputNum, &pOutput) != DXGI_ERROR_NOT_FOUND)
+		{
+			PIDXGIOutput pout(pOutput,Release());
+			vOutputs.push_back(pout);
+			++iOutputNum;
+		}
+		vector[i].screen_list.resize(iOutputNum);
+		
+		for (size_t n = 0; n < vOutputs.size(); n++)
+		{
+			// 获取显示设备信息  
+			DXGI_OUTPUT_DESC outputDesc;
+			vOutputs[n]->GetDesc(&outputDesc);
+			vector[i].screen_list[n] = core::StringFormat::WString2String(
+						std::wstring(outputDesc.DeviceName));
+			
+		}
+	}
+#endif
+	return vector;
+}
+
+
+
+}	//device_manager
+
+}	//rtplivelib
--- src/device_manager/gpudevice.h
+++ src/device_manager/gpudevice.h
@@ -3,24 +3,36 @@
 
 #include "../core/config.h"
 #include <string>
+#include <vector>
 
-//GPU设备
+namespace rtplivelib {
+
+namespace device_manager{
+
 //用于处理显卡信息
-struct GPUDevice{
-	std::string		name;
-	uint32_t		id;
-	uint64_t		video_memory;
-	uint64_t		system_memory;
-	uint64_t		shared_system_memory;
+struct GPUInfo{
+	/*显卡名字*/
+	std::string						name;
+	uint32_t						id;
+	uint64_t						video_memory;
+	uint64_t						system_memory;
+	/*该显卡输出的屏幕信息,size为0则是没有屏幕输出,目前只设置显示器名称*/
+	std::vector<std::string>		screen_list;
 };
 
-#ifdef WIN64
-#include <dxgi.h>
-#include <windows.h>
+class RTPLIVELIBSHARED_EXPORT GPUIdentify{
+public:
+	/**
+	 * @brief Get_All_GPU_Info
+	 * 获取所有显卡信息
+	 * @return 
+	 */
+	static std::vector<GPUInfo> Get_All_GPU_Info() noexcept;
 
-class RTPLIVELIBSHARED_EXPORT DXIG_GPU{
-	
 };
 
+}	//device_manager
 
-#endif
+}	//rtplivelib
+
+
